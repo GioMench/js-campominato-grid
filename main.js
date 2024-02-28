964283ca3b19bb65c9ecf1e98476415914614380
GioMench <giorgiaglm@gmail.com>
2024-02-28 22:00:06 +0100

    try change color

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let theBox = box.appendChild(element).classList.add('box');*/
 
 function createGrid(numberCell) {
 
+
     for (let i = 0; i < numberCell; i++) {
 
         const container = document.getElementById('container');
@@ -21,31 +22,62 @@ function createGrid(numberCell) {
 
 
     }
+
+
+
+    list = [];
+    list.push(cell)
+    console.log(list);
+
+    for (let i = 0; i < list.length; i++) {
+        const element = list[i];
+        console.log(element);
+        
+        element.addEventListener('click', function () {
+            element.classList.toggle('box2');
+            console.log(element);
+        })
+
+    }
+
+
+
+
 }
 
+
+
+
+
+
+
+/*list = []
+list.push(cell)
+console.log(list);
+
+for (let i = 0; i < list.length; i++) {
+    const element = list[i];
+    console.log(element);
+    cell.addEventListener('click', function (){
+        element.classList.toggle('box2');
+        console.log(element);
+    })
+    
+}*/
+
+
+
+/*for (let i = 0; i < cell.length; i++) {
+       var theCell = cell[i];
+       console.log(theCell);
+
+   }*/
 
 document.getElementById("play").addEventListener('click', function () {
 
 
     createGrid(100)
 
-    /*for (let i = 0; i < 100; i++) {
-
-        const container = document.getElementById('container');
-        var cell = document.createElement('div');
-        cell.classList.add('box');
-        container.appendChild(cell);
-        cell.append(i + 1);
-        console.log(cell);
-
-
-    }*/
-
-    /*for (let i = 0; i < cell.length; i++) {
-        var theCell = cell[i];
-        console.log(theCell);
-
-    }*/
 
 });
 
@@ -63,7 +95,7 @@ document.getElementById("btn_hard").addEventListener('click', function () {
     createGrid(49);
 
     container.style.width = "700px";
-    
+
 
 
 });

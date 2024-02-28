c17e626f3782cf0bbe3f14d58de954ea5aa9a6b3
GioMench <giorgiaglm@gmail.com>
2024-02-28 21:23:05 +0100

    add number and create a function for create grid. miss eventlistener for change color

--- main.js
+++ main.js
@@ -8,29 +8,66 @@
 var box = document.getElementById('container');
 let theBox = box.appendChild(element).classList.add('box');*/
 
+function createGrid(numberCell) {
 
-document.getElementById("play").addEventListener("click", function () {
+    for (let i = 0; i < numberCell; i++) {
 
-
-    const container = document.getElementById('container');
-
-
-    for (let i = 0; i < 100; i++) {
-
-        //const number = Math.floor((Math.random() * 100) + 1);
-       var cell = document.createElement('div');
+        const container = document.getElementById('container');
+        var cell = document.createElement('div');
         cell.classList.add('box');
         container.appendChild(cell);
+        cell.append(i + 1);
+        console.log(cell);
+
 
     }
+}
 
+
+document.getElementById("play").addEventListener('click', function () {
+
+
+    createGrid(100)
+
+    /*for (let i = 0; i < 100; i++) {
+
+        const container = document.getElementById('container');
+        var cell = document.createElement('div');
+        cell.classList.add('box');
+        container.appendChild(cell);
+        cell.append(i + 1);
+        console.log(cell);
+
+
+    }*/
+
+    /*for (let i = 0; i < cell.length; i++) {
+        var theCell = cell[i];
+        console.log(theCell);
+
+    }*/
+
+});
+
+document.getElementById("btn_medium").addEventListener('click', function () {
+
+    createGrid(81);
+
+    container.style.width = "900px";
+
+
+});
+
+document.getElementById("btn_hard").addEventListener('click', function () {
+
+    createGrid(49);
+
+    container.style.width = "700px";
     
 
-    
-      
-        
-    
-}); 
+
+});
+
 
 
 

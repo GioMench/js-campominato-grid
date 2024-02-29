1ebbf37775aed45d0e48abd890cd9a4f4b244424
GioMench <giorgiaglm@gmail.com>
2024-02-29 10:44:28 +0100

    add eventlistener for change color

--- main.js
+++ main.js
@@ -3,81 +3,42 @@
 //
 
 
-
-/*var element = document.createElement('div');
-var box = document.getElementById('container');
-let theBox = box.appendChild(element).classList.add('box');*/
-
+/**
+ * 
+ * create div element of grid
+ * append the element in container 
+ * add to element a class
+ * add evenlistenr for change color of cell and log message
+ * 
+ */ 
 function createGrid(numberCell) {
 
+    const container = document.getElementById('container');
 
     for (let i = 0; i < numberCell; i++) {
 
-        const container = document.getElementById('container');
-        var cell = document.createElement('div');
+        const cell = document.createElement('div');
         cell.classList.add('box');
         container.appendChild(cell);
         cell.append(i + 1);
         console.log(cell);
 
-
-    }
-
-
-
-    list = [];
-    list.push(cell)
-    console.log(list);
-
-    for (let i = 0; i < list.length; i++) {
-        const element = list[i];
-        console.log(element);
-        
-        element.addEventListener('click', function () {
-            element.classList.toggle('box2');
-            console.log(element);
+        cell.addEventListener('click', function () {
+            cell.classList.toggle('box2');
+            console.log(i+1, 'colpita');
         })
 
     }
 
-
-
-
 }
 
 
-
-
-
-
-
-/*list = []
-list.push(cell)
-console.log(list);
-
-for (let i = 0; i < list.length; i++) {
-    const element = list[i];
-    console.log(element);
-    cell.addEventListener('click', function (){
-        element.classList.toggle('box2');
-        console.log(element);
-    })
-    
-}*/
-
-
-
-/*for (let i = 0; i < cell.length; i++) {
-       var theCell = cell[i];
-       console.log(theCell);
-
-   }*/
-
 document.getElementById("play").addEventListener('click', function () {
 
-
-    createGrid(100)
-
+ 
+    
+    createGrid(100);
+    
 
 });
 
